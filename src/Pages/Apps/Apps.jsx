3c7e6d5958fe803f6ed1dfd5f660dac326bfae01
afzalhossainraju100 <raju23105341031@diu.edu.bd>
2026-01-09 23:45:05 +0600

    Data fetch using loader and displaied 8 data

--- src/Pages/Apps/Apps.jsx
+++ src/Pages/Apps/Apps.jsx
@@ -1,26 +1,32 @@
-import React from 'react';
-import { Suspense } from 'react';
-import App from '../App/App.jsx';
+import React from "react";
+import { Suspense } from "react";
+import App from "../App/App.jsx";
 
-const Apps = () => {
-    return (
-      <div className="flex flex-col w-[90%] justify-center items-center">
-        <div className="flex flex-col w-[90%] justify-center items-center p-10">
-          <h1 className="text-[#000000] text-[2.5rem] font-bold leading-15 text-center">
-            Trending Apps
-          </h1>
-          <p className="text-[#627382] text-[1.25rem] font-400  leading-15 text-center">
-            Explore All Trending Apps on the Market developed by us
-          </p>
-        </div>
-        <Suspense fallback={<div>Loading...</div>}>
-          <App></App>
-        </Suspense>
-        <button className="bg-gradient-to-r from-[#632EE3] to-[#9F62F2] text-white hover:from-[#9F62F2] hover:to-[#632EE3] border-0 px-6 py-3 rounded-md mb-10 shadow-md hover:shadow-lg text-[1rem] font-semibold">
-          Show All
-        </button>
+const Apps = ({ data }) => {
+  return (
+    <div className="flex flex-col w-[90%] justify-center items-center space-y-6 my-10 mx-auto">
+      <div className="flex flex-col w-[90%] justify-center items-center p-10">
+        <h1 className="text-[#000000] text-[2.5rem] font-bold leading-15 text-center">
+          Trending Apps
+        </h1>
+        <p className="text-[#627382] text-[1.25rem] font-400  leading-15 text-center">
+          Explore All Trending Apps on the Market developed by us
+        </p>
       </div>
-    );
+      <Suspense
+        fallback={<span className="loading loading-infinity loading-xl"></span>}
+      >
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mb-10">
+          {data.slice(0, 8).map((singleApp) => (
+            <App key={singleApp.id} singleApp={singleApp} />
+          ))}
+        </div>
+      </Suspense>
+      <button className="bg-gradient-to-r from-[#632EE3] to-[#9F62F2] text-white hover:from-[#9F62F2] hover:to-[#632EE3] border-0 px-6 py-3 rounded-md mb-10 shadow-md hover:shadow-lg text-[1rem] font-semibold">
+        Show All
+      </button>
+    </div>
+  );
 };
 
 export default Apps;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from '../../Components/Banner/Banner.jsx';
 import Apps from '../Apps/Apps.jsx';
+import { useLoaderData } from 'react-router-dom';
 
 const Home = () => {
+    const data = useLoaderData();
     return (
         <div>
             <Banner></Banner>
-            <Apps></Apps>
+            <Apps data={data}></Apps>
         </div>
     );
 };
